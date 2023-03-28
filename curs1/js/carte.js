51b8ad98b193d2f2330a8e4b24f411da1ad45a5f
Frontend JavaScript <frontendjavascript@C3-28.local>
2023-03-28 20:48:07 +0300

    curs1

--- curs1/js/carte.js
+++ curs1/js/carte.js
@@ -1,37 +1,59 @@
 class Carte {
-    constructor(titlu, autor, editura, an_aparitie, pret, nr_pagini){
+    static reducere = 10;
+    mypret;
+    constructor(titlu, autor, editura, an,nr_pagini){
     this.titlu = titlu;
     this.autor = autor;
     this.editura = editura;
-    this.an_aparitie = an_aparitie;
-    this.pret = pret;
+    this.an = an;
     this.nr_pagini = nr_pagini;
     }
-
-   obtine_titlu(){
-    console.log (`Titlul acestei carti este ${this.titlu}`);
-   }
-   obtine_autor(){
-    console.log (`Autorul acestei carti este ${this.autor}`);
-   }
-   modifica_pret(pretNou){
-    if(pretNou < 100 && pretNou > this.pret){
-        this.pret=pretNou
+    get pret(){
+        console.log('Sunt in getter');
+        return this.mypret;
+    }
+    set pret(pretNou){
+        console.log('Sunt in setter');
+        if (pretNou<= 0){
+            throw 'Pretul nu poate fi actualizat';
+        }
+        else{
+            this.mypret = pretNou;
+        }
+        
     }
 
-   }
-   reducere(procent){
-    // this.pret = this.pret - (procent/100*this.pret)
-    this.pret-=(procent*this.pret)/100;
-    console.log(`Pretul cu reducere este ${this.pret}`);
+//    obtine_titlu(){
+//     console.log (`Titlul acestei carti este ${this.titlu}`);
+//    }
+//    obtine_autor(){
+//     console.log (`Autorul acestei carti este ${this.autor}`);
+//    }
+//    modifica_pret(pretNou){
+//     if(pretNou < 100 && pretNou > this.pret){
+//         this.pret=pretNou
+//     }
 
-   }
-   afiseaza_info(){
+//    }
+//    reducere(procent){
+//     // this.pret = this.pret - (procent/100*this.pret)
+//     this.pret-=(procent*this.pret)/100;
+//     console.log(`Pretul cu reducere este ${this.pret}`);
+
+//    }
+   info(){
     console.log(`Cartea cu numele ${this.titlu} este scrisa de ${this.autor} in anul ${this.an_aparitie}, are un numar de ${this.nr_pagini} pagini si costa ${this.pret}`);
 
    }
-   deschide_cartea(){
-
+   deschidCartea(){
+    console.log ('Am deschis cartea la pagina 15');
+   }
+   randomIntFromInterval(min, max){
+    return Math.floor(Math.random()* (max-min +1)+ min)
+   }
+   pretNou(){
+    this.pret = this.pret -(Carte.reducere * this.pret)/100
    }
 }
 
+ 
